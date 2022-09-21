027e1401b076e490e22364087689e9d7c2a6d882
Lance Gee <Lancegee.apps@gmail.com>
2022-09-21 09:06:46 -0600

    added popper for bootstrap

--- next.config.js
+++ next.config.js
@@ -6,9 +6,4 @@ const nextConfig = {
 
 module.exports = {
   nextConfig,
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
 };
